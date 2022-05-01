f9b7ee05adfdd0fe89de3abd70165ea809a84fe9
Sasha Czerniawski <sashacandoit@gmail.com>
2022-05-01 19:02:23 -0400

    finally got new cupcake to add to the list

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -3,7 +3,7 @@ const BASE_URL = "http://127.0.0.1:5000/api";
 
 function generateCupcakeHTML(cupcake) {
     return `
-        <div id="${cupcake.id}" class="row mb-4">
+        <div data-cupcake-id="${cupcake.id}" class="row mb-4 cupcake-container">
             <div class="col-2">
                 <span class="img-icon">
                     <img class src="${cupcake.image}" alt="">
@@ -11,8 +11,9 @@ function generateCupcakeHTML(cupcake) {
             </div>
             <div class="col-6">
                 <h3 class="title">${cupcake.flavor}</h3>
-                <p><b>Rating:</b> ${cupcake.rating} | <b>Size:</b> ${cupcake.size}</p>
+                <p><b>Rating:</b> ${cupcake.rating} | <b>Size:</b> ${cupcake.size} | <a class="delete-button fw-bold text-danger" href="">delete</a></p>
             </div>
+
         </div>
     `;
 }
@@ -29,14 +30,37 @@ async function showAllCupcakes() {
 }
 
 
+/** handle form submition for adding new cupcake */
+
+$("#add-cupcake-form").on("submit", async function (evt) {
+    evt.preventDefault();
+
+    let flavor = $("#input-flavor").val();
+    let rating = $("#input-rating").val();
+    let size = $("#input-size").val();
+    let image = $("#input-image").val();
+
+    const newCupcakeRes = await axios.post(`${BASE_URL}/cupcakes`, {
+        flavor, rating, size, image
+    });
+    console.log(newCupcakeRes);
+
+    let newCupcake = $(generateCupcakeHTML(newCupcakeRes.data.new_cupcake));
+    $("#all-cupcakes").append(newCupcake);
+    $("#add-cupcake-form").trigger("reset");
+});
 
 
-$('.delete-cupcake').click(deleteCupcake)
+/** handle delete event for selected cupcake */
 
-async function deleteCupcake() {
-    const id = $(this).data('id')
-    await axios.delete(`/api/cupcakes/${id}`)
-    $(this).parent().remove()
-}
+$('#all-cupcakes').on("click", ".delete-button", async function (evt) {
+    evt.preventDefault();
+    let $cupcake = $(evt.target).closest("div.cupcake-container");
+    let cupcakeId = $cupcake.attr("data-cupcake-id");
+
+    await axios.delete(`${BASE_URL}/cupcakes/${cupcakeId}`);
+    $cupcake.remove();
+});
+
 
 $(showAllCupcakes);
